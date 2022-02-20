ac42f3b6160520115e04553cf4cc43d8ee36c1bc
Diana Lease <dianarlease@gmail.com>
2022-02-19 23:37:59 -0500

    feat(app): add home page and nav
    Signed-off-by: Diana Lease <dianalease@Dianas-MacBook-Pro.local>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Diana and Eetai</h1>
+          <div className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/schedule">Schedule</Link>
+            <Link to="/accomodations">Accomodations</Link>
+            <Link to="/directions">Directions</Link>
+          </div>
+          <div className="nav-mobile">
+            <span>Menu</span>
+          </div>
+        </header>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/schedule" element={<Home />}/>
+          <Route path="/accomodations" element={<Home />}/>
+          <Route path="/directions" element={<Home />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
